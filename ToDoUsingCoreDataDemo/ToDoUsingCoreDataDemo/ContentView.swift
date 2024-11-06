691b9ce43866b9ce7dd613abf91987b165da2f57
shafiqullah <shohagbit0404@gmail.com>
2024-11-06 14:32:47 -0800

    Stating Todo demo app using CoreData
    Signed-off-by: shafiqullah <shohagbit0404@gmail.com>

---
+++ ToDoUsingCoreDataDemo/ToDoUsingCoreDataDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoUsingCoreDataDemo
+//
+//  Created by Shafiq  Ullah on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoUsingCoreDataDemo/ToDoUsingCoreDataDemo/ToDoUsingCoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoUsingCoreDataDemoApp.swift
+//  ToDoUsingCoreDataDemo
+//
+//  Created by Shafiq  Ullah on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoUsingCoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
